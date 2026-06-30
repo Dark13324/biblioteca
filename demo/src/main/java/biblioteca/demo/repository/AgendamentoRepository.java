ed16f814464f6a458ddefb175cceb8d8d5f9ec1d
Dark13324 <victormiyashita@gmail.com>
2026-06-30 18:36:44 -0300

    criando o service

--- demo/src/main/java/biblioteca/demo/repository/AgendamentoRepository.java
+++ demo/src/main/java/biblioteca/demo/repository/AgendamentoRepository.java
@@ -4,6 +4,13 @@ import biblioteca.demo.model.Biblioteca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Biblioteca, Long> {
+
+    List<Biblioteca> findByquantidadeDisponivelGreaterThan(Integer quantidadeDisponivel);
+    Optional<Biblioteca> findBytituloContainingIgnoreCase(String titulo);
+
 }
--- demo/src/main/java/biblioteca/demo/service/LivroAgendamento.java
+++ demo/src/main/java/biblioteca/demo/service/LivroAgendamento.java
@@ -4,17 +4,28 @@ package biblioteca.demo.service;
 import biblioteca.demo.model.Biblioteca;
 import biblioteca.demo.repository.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class LivroAgendamento {
 
-  private final AgendamentoRepository agendamentoRepository;
+    @Autowired
+    private final AgendamentoRepository agendamentoRepository;
 
-  public Biblioteca save
+    public List<Biblioteca> consultarBiblioteca() {
+        return agendamentoRepository.findByquantidadeDisponivelGreaterThan(0);
 
+    }
 
+    public Biblioteca pesquisarLivro(String titulo) {
+        return agendamentoRepository.findBytituloContainingIgnoreCase(titulo)
+                .orElseThrow(() -> new RuntimeException("Livro não encontrado"));
+    }
 
 
 }
